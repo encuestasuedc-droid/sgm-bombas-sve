053452d94b92b545749caac39a684d496b9e2d56
encuestasuedc-droid <encuestas.uedc@gmail.com>
2026-09-22 11:55:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask
- 
+
 app = Flask(__name__)
- 
-@app.route('/')
+
+@app.route("/")
 def inicio():
-return """
-<h1>SGM Bombas SVE v1.0</h1>
-<h3>Sistema de Gestión de Mantenimiento</h3>
- 
-<ul>
-<li>C1 a C23</li>
-<li>Registro de reparaciones</li>
-<li>Historial</li>
-<li>Dashboard</li>
-</ul>
-"""
- 
+    return """
+    <h1>SGM Bombas SVE v1.0</h1>
+    <h3>Sistema de Gestión de Mantenimiento</h3>
+
+    <ul>
+        <li>C1 a C23</li>
+        <li>Registro de reparaciones</li>
+        <li>Historial</li>
+        <li>Dashboard</li>
+        <li>Ranking</li>
+    </ul>
+    """
+
 if __name__ == "__main__":
-app.run()
+    app.run(host="0.0.0.0", port=10000)
