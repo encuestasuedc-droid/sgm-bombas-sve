8362e111901f1709e329b61a54732292c7e838c0
encuestasuedc-droid <encuestas.uedc@gmail.com>
2026-09-22 12:46:25 -0300

    app.py

--- app.py
+++ app.py
@@ -1,69 +1,38 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
-
-# Base simple en memoria (paso previo a SQLite)
-registros = []
 
 @app.route("/")
 def inicio():
     return """
     <h1>SGM Bombas SVE v1.0</h1>
 
-    <p>/nuevo🛠 Registrar mantenimiento</a></p>
+    <p>✅ Aplicación funcionando</p>
 
-    <p><historial📋 Ver historial</a></p>
+    <p>
+        <a href="/nuevo">🛠 Registrar mantenimientop>
+
+    <p>
+        /historial📋 Ver historial</a>
+    </p>
     """
 
-
-@app.route("/nuevo", methods=["GET", "POST"])
+@app.route("/nuevo")
 def nuevo():
-
-    if request.method == "POST":
-
-        registros.append(
-            {
-                "fecha": request.form["fecha"],
-                "bomba": request.form["bomba"],
-                "componente": request.form["componente"],
-                "responsable": request.form["responsable"],
-                "costo": request.form["costo"],
-                "observacion": request.form["observacion"],
-            }
-        )
-
-        return """
-        <h2>✅ Registro guardado</h2>
-
-        /
-            Volver al inicio
-        </a>
-        """
-
-    opciones = ""
-
-    for i in range(1, 24):
-        opciones += f"<option>C{i}</option>"
-
-    return f"""
+    return """
     <h1>Nuevo mantenimiento</h1>
 
-    <form method='post'>
+    <p>Pantalla en construcción</p>
 
-        <p>Fecha</p>
-        <input type='date' name='fecha' required>
+    <a href="/">Volver</a@app.route("/historial")
+def historial():
+    return """
+    <h1>Historial</h1>
 
-        <p>Bomba</p>
-        <select name='bomba'>
-            {opciones}
-        </select>
+    <p>Historial en construcción</p>
 
-        <p>Componente</p>
-        <select name='componente'>
-            <option>Comando eléctrico</option>
-            <option>Cable y enchufe</option>
-            <option>Motor</option>
-            <option>Turbina</option>
-            <option>Cabezal</option>
-            <option>Rulemán</option>
-            <option>Estopada</option
+    <aolver</a>
+    """
+
+if __name__ == "__main__":
+    app.run()
