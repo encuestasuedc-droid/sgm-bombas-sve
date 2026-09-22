ca1e0b97eea95b13eda7920a8d6086d3cb3b5f78
encuestasuedc-droid <encuestas.uedc@gmail.com>
2026-09-22 12:39:09 -0300

    app.py

--- app.py
+++ app.py
@@ -1,42 +1,69 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+# Base simple en memoria (paso previo a SQLite)
+registros = []
 
 @app.route("/")
 def inicio():
     return """
     <h1>SGM Bombas SVE v1.0</h1>
 
-    <p>✅ Aplicación funcionando</p>
+    <p>/nuevo🛠 Registrar mantenimiento</a></p>
 
-    <p>
-        <a href="/nuevo">r mantenimiento</a>
-    </p>
-
-    <p>
-        <a href="/historialstorial</a>
-    </p>
+    <p><historial📋 Ver historial</a></p>
     """
 
-@app.route("/nuevo")
+
+@app.route("/nuevo", methods=["GET", "POST"])
 def nuevo():
-    return """
+
+    if request.method == "POST":
+
+        registros.append(
+            {
+                "fecha": request.form["fecha"],
+                "bomba": request.form["bomba"],
+                "componente": request.form["componente"],
+                "responsable": request.form["responsable"],
+                "costo": request.form["costo"],
+                "observacion": request.form["observacion"],
+            }
+        )
+
+        return """
+        <h2>✅ Registro guardado</h2>
+
+        /
+            Volver al inicio
+        </a>
+        """
+
+    opciones = ""
+
+    for i in range(1, 24):
+        opciones += f"<option>C{i}</option>"
+
+    return f"""
     <h1>Nuevo mantenimiento</h1>
 
-    <p>Pantalla en construcción</p>
+    <form method='post'>
 
-    <a href</a>
-    """
+        <p>Fecha</p>
+        <input type='date' name='fecha' required>
 
-@app.route("/historial")
-def historial():
-    return """
-    <h1>Historial</h1>
+        <p>Bomba</p>
+        <select name='bomba'>
+            {opciones}
+        </select>
 
-    <p>Historial en construcción</p>
-
-    /Volver</a>
-    """
-
-if __name__ == "__main__":
-    app.run()
+        <p>Componente</p>
+        <select name='componente'>
+            <option>Comando eléctrico</option>
+            <option>Cable y enchufe</option>
+            <option>Motor</option>
+            <option>Turbina</option>
+            <option>Cabezal</option>
+            <option>Rulemán</option>
+            <option>Estopada</option
