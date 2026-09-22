f9ac9ea7eb857cd3f30576af08854ac35a12d8a0
encuestasuedc-droid <encuestas.uedc@gmail.com>
2026-09-22 12:24:22 -0300

    app.py

--- app.py
+++ app.py
@@ -1,21 +1,121 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+registros = []
+
 @app.route("/")
 def inicio():
-    return """
-    <h1>SGM Bombas SVE v1.0</h1>
-    <h3>Sistema de Gestión de Mantenimiento</h3>
 
-    <ul>
-        <li>C1 a C23</li>
-        <li>Registro de reparaciones</li>
-        <li>Historial</li>
-        <li>Dashboard</li>
-        <li>Ranking</li>
-    </ul>
+    html = """
+    <h1>SGM Bombas SVE v1.0</h1>
+
+    <a href='/nuevoRegistrar mantenimiento
+    </a>
+
+    <br><br>
+
+    /historial
+        Ver historial
+    </a>
     """
 
+    return html
+
+
+@app.route("/nuevo", methods=["GET", "POST"])
+def nuevo():
+
+    if request.method == "POST":
+
+        bomba = request.form["bomba"]
+        componente = request.form["componente"]
+        observacion = request.form["observacion"]
+
+        registros.append({
+            "bomba": bomba,
+            "componente": componente,
+            "observacion": observacion
+        })
+
+        return """
+        <h2>Registro guardado</h2>
+
+        /
+        Volver
+        </a>
+        """
+
+    bombas = ""
+
+    for i in range(1, 24):
+        bombas += f"<option>C{i}</option>"
+
+    return f"""
+    <h1>Nuevo mantenimiento</h1>
+
+    <form method='post'>
+
+        <label>Bomba</label>
+
+        <select name='bomba'>
+            {bombas}
+        </select>
+
+        <br><br>
+
+        <label>Componente</label>
+
+        <select name='componente'>
+
+            <option>Comando eléctrico</option>
+            <option>Cable y enchufe</option>
+            <option>Motor</option>
+            <option>Turbina</option>
+            <option>Cabezal</option>
+            <option>Rulemán</option>
+            <option>Estopada</option>
+            <option>Pintado</option>
+            <option>Ruedas</option>
+
+        </select>
+
+        <br><br>
+
+        <textarea
+            name='observacion'
+            placeholder='Observaciones'
+        ></textarea>
+
+        <br><br>
+
+        <button>
+            Guardar
+        </button>
+
+    </form>
+    """
+
+
+@app.route("/historial")
+def historial():
+
+    html = "<h1>Historial</h1>"
+
+    for r in registros:
+
+        html += f"""
+        <p>
+        {r['bomba']} -
+        {r['componente']} -
+        {r['observacion']}
+        </p>
+        """
+
+    html += "<br>/Volver</a>"
+
+    return html
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run()
