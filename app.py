ece2de2133ba6c2cb5d806819c4fd9f3d088f572
encuestasuedc-droid <encuestas.uedc@gmail.com>
2026-09-23 08:58:26 -0300

    app.py

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bombas.db"
-
-db = SQLAlchemy(app)
-
-class Mantenimiento(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    bomba = db.Column(db.String(20))
-
-with app.app_context():
-    db.create_all()
-
 @app.route("/")
 def inicio():
-    return """
-    <h1>SGM Bombas SVE v1.0</h1>
-    <p>✅ SQLite funcionando</p>
-    """
+    return render_template("inicio.html")
+
+@app.route("/nuevo")
+def nuevo():
+    return render_template("registro.html")
+
+@app.route("/historial")
+def historial():
+    return render_template("historial.html")
 
 if __name__ == "__main__":
     app.run()
